4a1de81e2f7e0168c829854606e126e565cdda27
Danyel Suxdorf <danyel.suxdorf@t-online.de>
2019-12-07 22:44:14 +0100

    server alpha version

--- server/RestfulServer/server.py
+++ server/RestfulServer/server.py
@@ -3,6 +3,8 @@
 
 from flask import Flask, request, jsonify
 from dbcon import DBcon
+from pdfmanager import *
+#from qrreader import *
 
 # Init
 app = Flask(__name__)
@@ -23,17 +25,35 @@ def get_tests():
         return jsonify({'error': 'no content'}), 404
 
 
+@app.route('/api/v1.0/formdata', methods=['GET'])
+def get_forms():
+    data = db.getForms()
+    if data is not None:
+        return data, 200
+    else:
+        return jsonify({'error': 'no content'}), 404
+
+
 @app.route('/api/v1.0/test/<string:id>', methods=['GET'])
 def get_test(id):
     data = db.getTest(id)
     if data is not None:
         return data, 200
     else:
-        return jsonify({'error': 'no content'}), 500
+        return jsonify({'error': 'no content'}), 404
+
+
+@app.route('/api/v1.0/formdata/<string:id>', methods=['GET'])
+def get_form(id):
+    data = db.getForm(id)
+    if data is not None:
+        return data, 200
+    else:
+        return jsonify({'error': 'no content'}), 404
 
 
 @app.route('/api/v1.0/test', methods=['POST'])
-def new_vehicle():
+def new_test():
     data = request.data
     response = db.newTest(data)
     if response is not None:
@@ -41,6 +61,45 @@ def new_vehicle():
     else:
         return jsonify({'error': 'could not create'}), 500
 
+
+@app.route('/api/v1.0/formdata', methods=['POST'])
+def new_form():
+    data = request.data
+    response = db.newForm(data)
+    if response is not None:
+        return jsonify({'id': response}), 201
+    else:
+        return jsonify({'error': 'could not create'}), 500
+
+
+@app.route('/api/v1.0/formdata/<string:id>', methods=['POST'])
+def update_form(id):
+    data = request.data
+    response = db.updateForm(id, data)
+    if response is not None:
+        return jsonify({'update': 'ok'}), 200
+    else:
+        return jsonify({'error': 'could not update'}), 500
+
+
+@app.route('/api/v1.0/formdata/qr/<string:id>', methods=['POST'])
+def received_qr(id):
+    data = request.data
+    # check incoming qr data
+    # ToDo
+    #readPDF()
+    response = createPDF(id, data)
+    if response > 0:
+        return jsonify({'formular_data': 'not ok'}), 406
+    else:
+        return jsonify({'formular_data': 'ok'}), 200
+
+"""
+@app.route('/readqr', methods=['GET'])
+def readQRcode():
+    readQR()
+    return jsonify({'reading_qr': 'finished'}), 200
+"""
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
